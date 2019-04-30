0338a025215de9d583d20b86a96bffbf20dabe00
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-04-30 21:56:29 +0200

    FileWordAnalyzerTest class refactored.

--- test/com/codecool/filepartreader/FileWordAnalyzerTest.java
+++ test/com/codecool/filepartreader/FileWordAnalyzerTest.java
@@ -1,5 +1,6 @@
 package com.codecool.filepartreader;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -10,30 +11,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FileWordAnalyzerTest {
     private FilePartReader filePartReader = new FilePartReader();
-    private final String FILEPATH = "resources/test.txt";
-    private Integer from = 1;
-    private Integer to = 5;
+    private FileWordAnalyzer wordAnalyzer;
+
+    @BeforeEach
+    void setupFilePartReader() {
+        String FILEPATH = "resources/test.txt";
+        Integer from = 1;
+        Integer to = 5;
+        filePartReader.setup(FILEPATH, from, to);
+        wordAnalyzer = new FileWordAnalyzer(filePartReader);
+    }
 
     @Test
     void isWordListInAlphabeticalOrder() {
-        filePartReader.setup(FILEPATH, from, to);
-        FileWordAnalyzer wordAnalyzer = new FileWordAnalyzer(filePartReader);
         List<String> expectedWordList = new ArrayList<>(Arrays.asList("bluer", "canoe", "cat", "evening", "kayak",
                 "madam", "palindrome", "peep", "racecar", "redder", "redivider", "refer", "stats", "tenet", "yellower"));
         assertIterableEquals(expectedWordList, wordAnalyzer.getWordsOrderedAlphabetically());
     }
     @Test
     void doesWordListContainWordsWithGivenSubstringOnly() {
-        filePartReader.setup(FILEPATH, from, to);
-        FileWordAnalyzer wordAnalyzer = new FileWordAnalyzer(filePartReader);
         List<String> expectedWordList = new ArrayList<>(Arrays.asList("redivider", "yellower", "redder", "bluer", "refer"));
         assertIterableEquals(expectedWordList, wordAnalyzer.getWordsContainingSubstring("er"));
     }
 
     @Test
     void doesWordListConsistOfPalindromesOnly() {
-        filePartReader.setup(FILEPATH, from, to);
-        FileWordAnalyzer wordAnalyzer = new FileWordAnalyzer(filePartReader);
         List<String> expectedWordList = new ArrayList<>(Arrays.asList("redivider", "racecar", "redder", "madam", "kayak",
                 "refer", "stats", "tenet", "peep"));
         assertIterableEquals(expectedWordList, wordAnalyzer.getStringsWhichArePalindromes());
